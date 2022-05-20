e577ea5283016ee4925c72d6e7d3f3e60ef4ade9
VyctorCosta <vyctorcosta00@gmail.com>
2022-05-20 17:42:59 -0300

    style: HotFix background of card component

--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -2,15 +2,15 @@ import styled from "styled-components";
 
 export const Background = styled.div`
   background-color: ${({ theme }) => theme.Colors.Primary.mainBackground};
+  max-width: 144rem;
   width: 100%;
+  margin: 0 auto;
 `;
 
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(40rem, 1fr));
   gap: 4rem;
-  max-width: 144rem;
-  margin: 0 auto;
   padding: 0 8rem;
   padding-bottom: 10rem;
   background-color: ${({ theme }) => theme.Colors.Primary.mainBackground};
